53958fae44a7128c400010a8d5ff246375217b8e
ksoichiro <soichiro.kashima@gmail.com>
2014-08-22 00:58:29 +0900

    Tested applying plugin under 'android' plugin applied environment.

---
+++ samples/consumer/src/main/java/com/github/ksoichiro/gag/samples/comsumer/MainActivity.java
@@ -0,0 +1,16 @@
+package com.gihub.ksoichiro.gag.samples.consumer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
