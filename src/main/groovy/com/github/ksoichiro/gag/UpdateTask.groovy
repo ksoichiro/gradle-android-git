e845fb09837f36851b91ca5ef17151c344c5c47f
ksoichiro <soichiro.kashima@gmail.com>
2014-08-23 19:29:58 +0900

    Added checkout and update.

--- src/main/groovy/com/github/ksoichiro/gag/UpdateTask.groovy
+++ src/main/groovy/com/github/ksoichiro/gag/UpdateTask.groovy
@@ -7,26 +7,20 @@ class UpdateTask extends DefaultTask {
     @TaskAction
     def exec() {
         println "Dependencies managed by gag:"
-        project.git.dependencies.repos.each() { repo ->
+        project.git.dependencies.repos.each() { Repo repo ->
             println "dependency:"
             println "  location: ${repo.location}"
             println "  name: ${repo.name}"
             println "  commit: ${repo.commit}"
             println "  tag: ${repo.tag}"
             def repoDir = project.file('library/' + repo.name)
-            if (repoDir.exists()) {
-                // TODO Already cloned, so we discard changes and update
-            } else {
+            if (!repoDir.exists()) {
                 // This is the first time, so we clone
                 println "  Initialize dependency '${repo.name}' from '${repo.location}'..."
                 def cmd = "git clone ${repo.location} ./library/${repo.name}"
                 def proc = cmd.execute()
                 proc.waitFor()
-                // println "return code: ${proc.exitValue()}"
-                // println "stderr: ${proc.err.text}"
-                // println "stdout: ${proc.in.text}"
                 if (proc.exitValue() == 0) {
-                    // TODO Check out the appropriate version specified with commit or tag
                     println "  Completed successfully."
                     println "  To use this dependency, you need some actions:"
                     println "  * Add following lines to your `settings.gradle` in the root project directory."
@@ -45,7 +39,33 @@ class UpdateTask extends DefaultTask {
                     // TODO Exit with error
                 }
             }
+            checkout repo
         }
         println "Done. Please put `library/` to your .gitignore."
+    }
+
+    static def checkout(Repo repo) {
+        def version = repo.commit == null ? repo.tag : repo.commit;
+        def cmd = "cd library/${repo.name} && git checkout master"
+        def proc = cmd.execute()
+        proc.waitFor()
+
+        cmd = "cd library/${repo.name} && git fetch"
+        proc = cmd.execute()
+        proc.waitFor()
+
+        cmd = "cd library/${repo.name} && git checkout -f master"
+        proc = cmd.execute()
+        proc.waitFor()
+
+        cmd = "cd library/${repo.name} && git pull origin master"
+        proc = cmd.execute()
+        proc.waitFor()
+
+        if (version != null) {
+            cmd = "cd library/${repo.name} && git checkout ${version}"
+            proc = cmd.execute()
+            proc.waitFor()
+        }
     }
 }
